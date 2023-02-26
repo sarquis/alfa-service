096934fa50c0526cf301a7b20b64072d61960d1f
rsnf <roberto.sarquis@gmail.com>
2023-02-26 13:38:11 -0300

    Commit inicial.

---
+++ alfa-service/src/main/java/com/sqs/alfaservice/AlfaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sqs.alfaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfaServiceApplication.class, args);
+	}
+
+}
---
+++ alfa-service/src/test/java/com/sqs/alfaservice/AlfaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqs.alfaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
