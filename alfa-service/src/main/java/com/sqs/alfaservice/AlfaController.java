8e2344d839e3c4027157e85ae0017fd99c856886
rsnf <roberto.sarquis@gmail.com>
2023-03-07 21:16:23 -0300

    debug prop

--- alfa-service/src/main/java/com/sqs/alfaservice/AlfaController.java
+++ alfa-service/src/main/java/com/sqs/alfaservice/AlfaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AlfaController {
 
-    @GetMapping("/")
+    @GetMapping("/alfa")
     public String executar() {
 	return "MS-ALFA-RESPOSTA 0";
     }
